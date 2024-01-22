abeb35009d9a4cc5c111d634024ba1bc4b626f8e
dmitry3e3 <dvolkov0422@gmail.com>
2024-01-22 10:05:05 +0300

    Добавлены комментарии внутри кода

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { Component} from "@angular/core";
 import { FormsModule }   from "@angular/forms";
 
-
+//Обьявление модели данных
 class Item{
   name: string;
   description: string;
@@ -26,34 +26,34 @@ class Item{
 export class AppComponent {
     name: string = "";
     description: string = "";
-
+    //Заполнение первой записи на странице
     items: Item[] =
     [
         { name:"21.01.2023", description: "Моя первая запись"},
     ];
-
+    //Функция добавления новой записи в дневнике
     addItem(name: string , description: string): void {
 
         if(name==null || description.trim()=="")
             return;
         this.items.push(new Item(name, description));
     }
-
+    //функция удаления записи из дневника
     remove(index: number): void {
       this.items.splice(index, 1);
     }
-
+    //функция открытия модельного окна с формой
     openModal() {
       const modelDiv = document.getElementById('myModal');
       if (modelDiv != null){
         modelDiv.style.display = 'block';
       }
     }
+    //функция закрытия модального окна с формой
     closeModal() {
       const modelDiv = document.getElementById('myModal');
       if (modelDiv != null){
         modelDiv.style.display = 'none';
       }
     }
-
 }
